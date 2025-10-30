33f5e057585598dcbe9ec6551a477b43f8f39dc4
unknown <oa_meznaoui@esi.dz>
2025-10-30 23:22:02 +0100

    AppBar done

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 
 class home extends StatelessWidget {
@@ -7,12 +9,39 @@ class home extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar:AppBar(
+        backgroundColor: Colors.white,
+        leading:  IconButton(
+          onPressed: () {},
+          icon: Image.asset('assets/images/menuicon.png'),
+         
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications),
+          )
+        ],
+        title:Text('Runway'),
+        titleTextStyle:GoogleFonts.inter(
+          fontWeight:FontWeight.bold ,
+          fontSize: 18,
+          letterSpacing:0,
+          color: Colors.black,
+
+
+         ),
+          centerTitle:true,
+
+        ) ,
+      );
 
 
 
 
 
-    );
+
 
 
     }
