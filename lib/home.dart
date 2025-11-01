adc64b69cd6a152c4088a365b121b6c9ab8446b9
unknown <oa_meznaoui@esi.dz>
2025-11-01 08:49:17 +0100

    fixed video layout + bottomnavigationbar

--- lib/home.dart
+++ lib/home.dart
@@ -17,11 +17,13 @@ class _homeState extends State<home> {
   @override
   void initState() {
     // TODO: implement initState
+    final int _selectedindex =0;
     _controller=VideoPlayerController.asset('assets/videos/video.mp4')
     ..initialize().then((_) {
     // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
     setState(() {
       _controller.play();
+      _controller.setLooping(true);
 
     });
     });
@@ -35,18 +37,52 @@ class _homeState extends State<home> {
       backgroundColor: Colors.white,
       appBar: customappbar( title:'Runway',prefix: 'assets/images/menuicon.png', suffix: 'assets/images/notif.png'),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        scrollDirection: Axis.vertical,
+        child: Stack(
           children: [
-            AspectRatio(
-               aspectRatio: 0.9,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AspectRatio(
+                   aspectRatio: 0.9,
 
-                child: VideoPlayer(_controller)),
-            const SizedBox(height: 20),
-            const SizedBox(height: 20),
+                    child: Transform.scale(
+                      scale: 1.5,
+
+                        child: VideoPlayer(_controller))),
+                const SizedBox(height: 20),
+                const SizedBox(height: 20),
+              ],
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+                right: 0,
+                child:Container(
+
+
+                ) )
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFFFFFFF),
+        selectedFontSize:,
+        
+        selectedItemColor: Colors.black,
+        currentIndex: 0,
+        onTap: (index){
+          
+        },
+
+          items:[
+            BottomNavigationBarItem(icon: Icon(Icons.home_filled ),label: 'Home'),
+            BottomNavigationBarItem(icon: Image.asset('assets/images/heart.png'),label: 'Likes'),
+            BottomNavigationBarItem(icon: Image.asset('assets/images/bag.png'),label: 'Bag'),
+            BottomNavigationBarItem(icon: Image.asset('assets/images/profile.png'),label: 'Profile'),
+
+            
+          ]),
     );
   }
 }
